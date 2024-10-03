a4861c471be55b44f59d4af858ebdd72f3de7407
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-10-03 18:54:50 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,6 @@ class MyApp extends StatelessWidget {
         title: 'Facebook',
         theme: ThemeData(primaryColor: Colors.white),
         debugShowCheckedModeBanner: false,
-        home: const Maintab());
+        home: Maintab());
   }
 }
