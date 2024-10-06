ed16e206a64c57afe3b8996b3d9102b2b2cd6b16
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-10-06 21:07:01 +0500

    first

--- lib/widget/post.dart
+++ lib/widget/post.dart
@@ -5,47 +5,65 @@ class Post extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            IconButton(
-                iconSize: 50,
-                onPressed: () => {},
-                icon: const CircleAvatar(
-                  radius: 80,
-                  backgroundImage: AssetImage('images/man7.jpeg'),
-                )),
-            const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Khan',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Wrap(
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              IconButton(
+                  iconSize: 50,
+                  onPressed: () => {},
+                  icon: const CircleAvatar(
+                    radius: 80,
+                    backgroundImage: AssetImage('images/man7.jpeg'),
+                  )),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      '12 min ago',
-                      style: TextStyle(fontSize: 18.0),
+                      'Khan',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    Icon(Icons.public),
+                    Wrap(
+                      children: [
+                        Text(
+                          '12 min ago',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        Icon(Icons.public),
+                      ],
+                    )
                   ],
-                )
+                ),
+              ),
+              IconButton(
+                iconSize: 30,
+                onPressed: () => {},
+                icon: Icon(Icons.more_horiz_outlined),
+              )
+            ],
+          ),
+          Container(
+            child: Column(
+              children: [
+                Text(
+                  'This is title',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black,
+                  ),
+                ),
+                Image(image: AssetImage('images/man6.jpeg'))
               ],
             ),
-            IconButton(
-              iconSize: 30,
-              onPressed: () => {},
-              icon: Icon(Icons.more_horiz_outlined),
-            )
-          ],
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
