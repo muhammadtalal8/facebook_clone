fd760a5b6de6678d9b7c67bf86e626fe38b4e7b1
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-10-07 18:10:50 +0500

    first

--- lib/widget/post.dart
+++ lib/widget/post.dart
@@ -6,85 +6,115 @@ class Post extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              IconButton(
-                  iconSize: 50,
-                  onPressed: () => {},
-                  icon: const CircleAvatar(
-                    radius: 80,
-                    backgroundImage: AssetImage('images/man7.jpeg'),
-                  )),
-              const Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Khan',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Wrap(
-                      children: [
-                        Text(
-                          '12 min ago',
-                          style: TextStyle(fontSize: 18.0),
-                        ),
-                        Icon(Icons.public),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              IconButton(
-                iconSize: 30,
+    Column(
+      children: [
+        Row(
+          children: [
+            IconButton(
+                iconSize: 50,
                 onPressed: () => {},
-                icon: const Icon(Icons.more_horiz_outlined),
-              )
-            ],
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'This is title',
+                icon: const CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage('images/man7.jpeg'),
+                )),
+            const Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Khan',
                     style: TextStyle(
-                      fontSize: 18.0,
+                      fontSize: 20.0,
                       color: Colors.black,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                Image(image: AssetImage('images/man6.jpeg'))
-              ],
-            ),
-          ),
-          Row(
-            children: [
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () => {},
-                    icon: Icon(Icons.thumb_up_alt_outlined),
-                  ),
-                  Text(
-                    "12",
-                    style: TextStyle(fontSize: 18.0),
+                  Wrap(
+                    children: [
+                      Text(
+                        '12 min ago',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      Icon(Icons.public),
+                    ],
                   )
                 ],
-              )
+              ),
+            ),
+            IconButton(
+              iconSize: 30,
+              onPressed: () => {},
+              icon: const Icon(Icons.more_horiz_outlined),
+            )
+          ],
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'This is title',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Image(image: AssetImage('images/man6.jpeg'))
             ],
-          )
-        ],
-      ),
-    );
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () => {},
+                  icon: const Icon(Icons.thumb_up_alt_outlined),
+                ),
+                const Text(
+                  "12",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                 Row(
+              children: [
+                IconButton(
+                  onPressed: () => {},
+                  icon: const Icon(Icons.message_outlined),
+                ),
+                const Text(
+                  "12",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                 Row(
+              children: [
+                IconButton(
+                  onPressed: () => {},
+                  icon: const Icon(Icons.share_outlined),
+                ),
+                const Text(
+                  "15",
+                  style: TextStyle(fontSize: 18.0),
+                )
+    
+              ],
+            )
+          ],
+        )
+      ],
+    ),
+          ]
+        )
+          ]
+        
+        ),
+    
+      ]
+    
+        );
   }
 }
