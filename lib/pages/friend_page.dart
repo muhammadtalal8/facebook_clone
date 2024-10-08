397834a83bc34bf846e6553d3142a6d5fb1fa922
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-10-08 17:52:39 +0500

    first

--- lib/pages/friend_page.dart
+++ lib/pages/friend_page.dart
@@ -10,9 +10,50 @@ class FriendPage extends StatefulWidget {
 class _FriendPageState extends State<FriendPage> {
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'friendpage',
-      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            "Friend",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                child: IconButton(
+                  icon: const Icon(Icons.person),
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  color: Colors.green,
+                  onPressed: () {
+                    print('Person CLicked');
+                  },
+                ),
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: Colors.grey[300]),
+              ),
+              Container(
+                child: IconButton(
+                  icon: const Icon(Icons.people),
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  color: Colors.redAccent,
+                  onPressed: () {
+                    print('Person CLicked');
+                  },
+                ),
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: Colors.grey[300]),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
