c4c35cca5efa68cb06a070cd2e25200aa21b0789
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-10-09 13:50:56 +0500

    first Commit

--- lib/pages/friend_page.dart
+++ lib/pages/friend_page.dart
@@ -105,7 +105,7 @@ class _FriendPageState extends State<FriendPage> {
                                       textStyle: const TextStyle(
                                           fontSize: 16,
                                           fontWeight: FontWeight.bold)),
-                                  child: const Text('Remove'))
+                                  child: Text('Remove'))
                             ],
                           ),
                           onTap: () {
